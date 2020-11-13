5017e2ae574dc92a8ed117d63e19e34547ecb80d
Matt Calhoun <matt@mattcalhoun.com>
2020-11-13 16:00:08 -0500

    initial module

--- main.tf
+++ main.tf
@@ -1,13 +1,89 @@
-resource "random_integer" "example" {
-  count = module.this.enabled ? 1 : 0
+#-----------------------------------------------------------------------------------------------------------------------
+# Subscribe the Acccount to GuardDuty
+#-----------------------------------------------------------------------------------------------------------------------
+resource "aws_guardduty_detector" "guardduty" {
+  enable                       = module.this.enabled
+  finding_publishing_frequency = var.finding_publishing_frequency
+}
 
-  min = 1
-  max = 50000
-  keepers = {
-    example = var.example
+#-----------------------------------------------------------------------------------------------------------------------
+# Optionally configure Event Bridge Rules and SNS subscriptions 
+# https://docs.aws.amazon.com/securityhub/latest/userguide/securityhub-cwe-integration-types.html
+# https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/resource-based-policies-cwe.html#sns-permissions
+#-----------------------------------------------------------------------------------------------------------------------
+module "sns_topic" {
+
+  source  = "cloudposse/sns-topic/aws"
+  version = "0.9.0"
+  count   = local.create_sns_topic ? 1 : 0
+
+  attributes      = ["guardduty"]
+  subscribers     = var.subscribers
+  sqs_dlq_enabled = false
+
+  context = module.this.context
+}
+
+module "findings_label" {
+  source  = "cloudposse/label/null"
+  version = "0.21.0"
+
+  attributes = ["guardduty-findings"]
+  context    = module.this.context
+}
+
+resource "aws_sns_topic_policy" "sns_topic_publish_policy" {
+  count  = module.this.enabled && local.create_sns_topic ? 1 : 0
+  arn    = local.findings_notification_arn
+  policy = data.aws_iam_policy_document.sns_topic_policy[0].json
+}
+
+data "aws_iam_policy_document" "sns_topic_policy" {
+  count     = module.this.enabled && local.create_sns_topic ? 1 : 0
+  policy_id = "GuardDutyPublishToSNS"
+  statement {
+    sid = ""
+    actions = [
+      "sns:Publish"
+    ]
+    principals {
+      type        = "Service"
+      identifiers = ["cloudwatch.amazonaws.com"]
+    }
+    resources = [module.sns_topic[0].sns_topic.arn]
+    effect    = "Allow"
   }
 }
 
+resource "aws_cloudwatch_event_rule" "findings" {
+  count       = local.enable_notifications == true ? 1 : 0
+  name        = module.findings_label.id
+  description = "GuardDuty Findings"
+  tags        = module.this.tags
+
+  event_pattern = jsonencode(
+    {
+      "source" : [
+        "aws.guardduty"
+      ],
+      "detail-type" : [
+        var.cloudwatch_event_rule_pattern_detail_type
+      ]
+    }
+  )
+}
+
+resource "aws_cloudwatch_event_target" "imported_findings" {
+  count = local.enable_notifications == true ? 1 : 0
+  rule  = aws_cloudwatch_event_rule.findings[0].name
+  arn   = local.findings_notification_arn
+}
+
+#-----------------------------------------------------------------------------------------------------------------------
+# Locals and Data References
+#-----------------------------------------------------------------------------------------------------------------------
 locals {
-  example = format("%v %v", var.example, join("", random_integer.example[*].result))
+  enable_notifications      = module.this.enabled && (var.create_sns_topic || var.findings_notification_arn != null)
+  create_sns_topic          = module.this.enabled && var.create_sns_topic
+  findings_notification_arn = local.enable_notifications ? (var.findings_notification_arn != null ? var.findings_notification_arn : module.sns_topic[0].sns_topic.arn) : null
 }
--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,9 @@
-output "id" {
-  description = "ID of the created example"
-  value       = module.this.enabled ? module.this.id : null
+output "sns_topic" {
+  description = "The SNS topic that was created"
+  value       = local.create_sns_topic ? module.sns_topic[0].sns_topic : null
 }
 
-output "example" {
-  description = "Example output"
-  value       = module.this.enabled ? local.example : null
-}
-
-output "random" {
-  description = "Stable random number for this example"
-  value       = module.this.enabled ? join("", random_integer.example[*].result) : null
+output "sns_topic_subscriptions" {
+  description = "The SNS topic that was created"
+  value       = local.create_sns_topic ? module.sns_topic[0].aws_sns_topic_subscriptions : null
 }
